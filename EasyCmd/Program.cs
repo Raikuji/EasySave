2d1242729bd68bec5d6e5a6002a3f65a8cc246ba
Antoine GACHENOT <irub.tech@gmail.com>
2025-01-28 14:31:16 +0100

    Add easysave cmd structure

---
+++ EasyCmd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCmd;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to EasySave");
+        Console.ReadKey();
+    }
+}
