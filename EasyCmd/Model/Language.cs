394af3162c827470d5f7f0409ef780cc6f098b66
Antoine GACHENOT <irub.tech@gmail.com>
2025-02-04 15:28:29 +0100

    Fix language model

---
+++ EasyCmd/Model/Language.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCmd.Model
+{
+    // Class representing the language manager using the Singleton pattern.
+    internal class Language
+    {
+        // Unique instance of the class (Singleton)
+        private static Language _instance;
+
+        // Associated language dictionary
+        private LanguageDictionary _languageDictionary;
+
+        // Private constructor to prevent external instantiation (Singleton).
+        private Language()
+        {
+            _languageDictionary = new LanguageDictionary();
+        }
+
+        // Returns the unique instance of the Language class.
+        // returns: Unique instance of Language
+        public static Language GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Language();
+            }
+            return _instance;
+        }
+
+        // Sets the language dictionary to be used.
+        // param languageDictionary: Instance of the language dictionary
+        public void SetLanguage(LanguageDictionary language)
+        {
+            _languageDictionary = language;
+        }
+        
+        public string GetString(string key)
+        {
+            return _languageDictionary.GetString(key);
+        }
+    }
+}
--- EasyCmd/Model/LanguageDictionary.cs
+++ EasyCmd/Model/LanguageDictionary.cs
@@ -1,63 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 // Class representing a language dictionary that stores translations as key/value pairs.
 public class LanguageDictionary
 {
-	// Private dictionary containing translations
-	private Dictionary<string, string> _dictionary;
+    // Private dictionary containing translations
+    private Dictionary<string, string> _dictionary;
 
-	// Constructor that initializes an empty dictionary.
-	public LanguageDictionary()
-	{
-		_dictionary = new Dictionary<string, string>();
-	}
+    // Constructor that initializes an empty dictionary.
+    public LanguageDictionary()
+    {
+        _dictionary = new Dictionary<string, string>();
+    }
 
-	// Retrieves the translation associated with a given key.
-	// If the key does not exist, it returns the key itself.
-	// param key: Translation key
-	// returns: Translated value if available, otherwise the key
-	public string GetString(string key)
-	{
-		return _dictionary.ContainsKey(key) ? _dictionary[key] : key;
-	}
+    // Retrieves the translation associated with a given key.
+    // If the key does not exist, it returns the key itself.
+    // param key: Translation key
+    // returns: Translated value if available, otherwise the key
+    public string GetString(string key)
+    {
+        return _dictionary.ContainsKey(key) ? _dictionary[key] : key;
+    }
 
-	// Adds a new translation or updates an existing one.
-	// param key: Translation key
-	// param value: Translated value
-	public void AddString(string key, string value)
-	{
-		_dictionary[key] = value;
-	}
-}
+    // Adds a new translation or updates an existing one.
+    // param key: Translation key
+    // param value: Translated value
+    public void AddString(string key, string value)
+    {
+        _dictionary.Add(key, value);
+    }
 
-// Class representing the language manager using the Singleton pattern.
-public class Language
-{
-	// Unique instance of the class (Singleton)
-	private static Language _instance;
-
-	// Associated language dictionary
-	private LanguageDictionary _languageDictionary;
-
-	// Private constructor to prevent external instantiation (Singleton).
-	private Language() { }
-
-	// Returns the unique instance of the Language class.
-	// returns: Unique instance of Language
-	public static Language GetInstance()
-	{
-		if (_instance == null)
-		{
-			_instance = new Language();
-		}
-		return _instance;
-	}
-
-	// Sets the language dictionary to be used.
-	// param languageDictionary: Instance of the language dictionary
-	public void SetLanguage(LanguageDictionary language)
-	{
-		_languageDictionary = language;
-	}
+    public void LoadLanguage(string jsonFilePath)
+    {
+        string stringLang = File.ReadAllText(jsonFilePath);
+        foreach (JsonElement jsonElement in JsonDocument.Parse(stringLang).RootElement.EnumerateArray())
+        {
+            foreach (JsonProperty jsonProperty in jsonElement.EnumerateObject())
+            {
+                AddString(jsonProperty.Name, jsonProperty.Value.GetRawText().Replace("\"", "").Replace("\\n", Environment.NewLine));
+            }
+        }
+    }
 }
