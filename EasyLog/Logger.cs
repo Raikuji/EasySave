67dc22fff13ae922c4cce6f22ecea5e12bcee247
Antoine GACHENOT <irub.tech@gmail.com>
2025-02-06 10:45:38 +0100

    Add Logger with .NET 8 compatibilities

---
+++ EasyLog/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace EasyLog
+{
+    public class Logger
+    {
+        private readonly string _logFilePath;
+
+        public Logger(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public void Log(Dictionary<string, object> data)
+        {
+            if (File.Exists(_logFilePath))
+            {
+                File.AppendAllText(_logFilePath, $",{Environment.NewLine}");
+            }
+            string json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.AppendAllText(_logFilePath, json);
+        }
+    }
+}
