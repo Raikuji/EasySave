ad29904b63b9782fd75a5ae9a0bc36fa19ef2029
JeremyH8 <jerem.houel@gmail.com>
2025-02-12 10:13:29 +0100

    1ère version log format xaml/json

---
+++ EasyLog/Properties/test.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Linq;
+using System.Xaml;
+
+namespace EasyLog
+{
+    public enum LogFormat
+    {
+        JSON,
+        XAML
+    }
+
+    public class Logger
+    {
+        private readonly string _logFilePath;
+        private readonly LogFormat _format;
+
+        public Logger(string logFilePath, LogFormat format)
+        {
+            _logFilePath = logFilePath;
+            _format = format;
+        }
+
+        public void Log(Dictionary<string, object> data)
+        {
+            string output;
+            if (_format == LogFormat.JSON)
+            {
+                output = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            }
+            else // XAML
+            {
+                output = SerializeToXaml(data);
+            }
+
+            File.AppendAllText(_logFilePath, output + Environment.NewLine);
+        }
+
+        private string SerializeToXaml(Dictionary<string, object> data)
+        {
+            var root = new XElement("Log");
+            foreach (var kvp in data)
+            {
+                root.Add(new XElement(kvp.Key, kvp.Value));
+            }
+            return XamlServices.Save(root);
+        }
+    }
+}
