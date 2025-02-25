57d9839f346ef86feeab55ee1d87f2c1ff15bfc1
Messaoud SABRI <messaoud.sabri@viacesi.fr>
2025-02-25 19:37:16 +0100

    Add transfer files limits

--- EasyGui/MainWindow.xaml.cs
+++ EasyGui/MainWindow.xaml.cs
@@ -1,27 +1,22 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using EasyCmd.Model;
 using EasyGui.ViewModels;
 
 namespace EasyGui
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			DataContext = MainWindowViewModel.Instance;
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = MainWindowViewModel.Instance;
+        }
 
         private void Frame_Navigated(object sender, NavigationEventArgs e)
         {
@@ -31,6 +26,20 @@ namespace EasyGui
         private void Frame_Navigated_1(object sender, NavigationEventArgs e)
         {
 
+        }
+
+        private void SaveMaxFileSize_Click(object sender, RoutedEventArgs e)
+        {
+            if (long.TryParse(MaxFileSizeTextBox.Text, out long maxFileSizeInKB))
+            {
+                // Create a BackupJob instance with the new value
+                BackupJob backupJob = new BackupJob("Nom", "Source", "Destination", 1, maxFileSizeInKB);
+                // Use the BackupJob instance as needed
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer une valeur valide pour la taille maximale des fichiers.");
+            }
         }
     }
 }
